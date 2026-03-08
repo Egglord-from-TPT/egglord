0ce8f2ad729b51107ec453d06f007b317eebeaf4
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-09 00:37:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="egglord",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="A simple Python library that motivates you every 10-120 seconds.",
     author="egglord",
